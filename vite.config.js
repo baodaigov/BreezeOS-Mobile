3effe1eef6a0db7caea9e1d41a73dcf6e3735614
daothanhminh <khangbietcode1000@gmail.com>
2023-07-13 02:50:01 +0700

    Commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BreezeOS-Mobile',optimizeDeps: {
-    exclude: ['react-swipeable']
+  base: '/BreezeOS-Mobile',
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   }
 })
