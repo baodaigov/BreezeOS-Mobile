2b60f599184454b2336c715eddfbae1d977fa2d9
daothanhminh <khangbietcode1000@gmail.com>
2023-10-11 01:15:58 +0700

    Commit

--- src/components/BatteryLevel.tsx
+++ src/components/BatteryLevel.tsx
@@ -1,33 +1,36 @@
 import { useEffect } from "react";
 import { useBattery } from "react-use";
-import { setBatteryCharging, setBatteryLevel } from "../store/reducers/settings";
+import {
+  setBatteryCharging,
+  setBatteryLevel,
+} from "../store/reducers/settings";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 interface BatteryLevelProps {
-    className?: string
+  className?: string;
 }
 
-export default function BatteryLevel({ className }: BatteryLevelProps){
-    const dispatch = useAppDispatch();
-    const battery = useAppSelector(state => state.settings.battery);
+export default function BatteryLevel({ className }: BatteryLevelProps) {
+  const dispatch = useAppDispatch();
+  const battery = useAppSelector((state) => state.settings.battery);
 
-    const batteryState = useBattery();
+  const batteryState = useBattery();
 
-    let batteryPercent = Math.round(batteryState.level * 100);
+  let batteryPercent = Math.round(batteryState.level * 100);
 
-    useEffect(() => {
-        if(isNaN(batteryPercent)){
-            dispatch(setBatteryLevel('-'));
-        } else {
-            dispatch(setBatteryLevel(batteryPercent));
-        }
+  useEffect(() => {
+    if (isNaN(batteryPercent)) {
+      dispatch(setBatteryLevel("-"));
+    } else {
+      dispatch(setBatteryLevel(batteryPercent));
+    }
 
-        if(batteryState.charging){
-            dispatch(setBatteryCharging(true));
-        } else {
-            dispatch(setBatteryCharging(false));
-        }
-    }, [batteryState]);
+    if (batteryState.charging) {
+      dispatch(setBatteryCharging(true));
+    } else {
+      dispatch(setBatteryCharging(false));
+    }
+  }, [batteryState]);
 
-    return <p className={className}>{battery.level}%</p>
+  return <p className={className}>{battery.level}%</p>;
 }
